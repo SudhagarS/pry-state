2c78a10c13669a95b4f99fa68f1ba96d3561fe2d
Brian Bugh <brainbag@gmail.com>
2017-07-04 12:43:01 -0500

    Account for developer .pryrc setting

--- spec/lib/pry-state/commands/toggle_state_spec.rb
+++ spec/lib/pry-state/commands/toggle_state_spec.rb
@@ -2,22 +2,24 @@ require "spec_helper"
 
 RSpec.describe "toggle-state" do
   context "when the hook is enabled" do
-    before { Pry.config.state_hook_enabled = true }
+    before { allow(Pry.config).to receive(:state_hook_enabled).and_return(true) }
 
     it "sets the hook config to be disabled" do
+      expect(Pry.config).to receive(:state_hook_enabled=).with(false)
       output = capture_pry_command("toggle-state", binding)
       expect(output).to include "pry-state disabled."
-      expect(Pry.config.state_hook_enabled).to eq false
     end
   end
 
   context "when the hook is disabled" do
-    before { Pry.config.state_hook_enabled = false }
+    before { allow(Pry.config).to receive(:state_hook_enabled).and_return(false) }
 
     it "sets the hook config to be enabled" do
+      # If the developer has `Pry.config.state_hook_enabled = true` in their
+      # .pryrc, this expectation will get called twice; take that into account
+      expect(Pry.config).to receive(:state_hook_enabled=).with(true).at_least(:once)
       output = capture_pry_command("toggle-state", binding)
       expect(output).to include "pry-state enabled."
-      expect(Pry.config.state_hook_enabled).to eq true
     end
   end
 end
